5c37189ce37e45ba4534f06c26021acb923138ea
too11451451 <849533752@qq.com>
2021-03-13 13:36:05 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -182,6 +182,17 @@ sudo iptables -t nat -A PREROUTING -i ${IFACE} -p tcp --dport 80 -j REDIRECT --t
   && sudo iptables -A INPUT -p icmp -j ACCEPT\
   && sudo iptables -A INPUT -i lo -j ACCEPT\
   && sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 5201 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 11000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m state --state NEW -m udp --dport 11000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 12222 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m state --state NEW -m udp --dport 12222 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 30000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m state --state NEW -m udp --dport 30000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 42350 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m state --state NEW -m udp --dport 42350 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 50123 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m state --state NEW -m udp --dport 50123 -j ACCEPT\
   && sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT\
   && sudo iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT\
   && sudo iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT\
